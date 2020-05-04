d6416880d35b64e4f0cab88bb950891636ae09db
piabedinicrocci <piabedinicrocci@hotmail.com>
2020-05-04 20:57:00 +0200

    todo

--- js/captcha.js
+++ js/captcha.js
@@ -1,37 +1,26 @@
-document.addEventListener("DOMContentLoaded", initJS);
+"use strict";
 
-function initJS() {
-    "use strict";
-
-    function createCaptcha() {
-        let captcha = [];
-        let charArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
-        for (let i = 0; i < max_captcha; i++) {
-            let random = Math.floor(Math.random() * charArray.length + 1);
-            captcha.push(charArray[random]);
-        }
-        let valueCaptcha = document.getElementById("captcha");
-        valueCaptcha.innerHTML = captcha.join("");
-        valueCaptcha.value = captcha.join("");
-    }
-
-    function checkCaptcha() {
-        let genCaptcha = document.getElementById("captcha");
-        let result = document.getElementById("captchaResult");
-        if (genCaptcha.value == inputCaptcha.value) {
-            result.innerHTML = "VALID CAPTCHA";
-        }
-        else {
-            result.innerHTML = "INVALID CAPTCHA";
-            document.getElementById("form").reset();
-            createCaptcha();
-        }
-    }
-
-    const max_captcha = 5;
-
-    createCaptcha();
-
-    let inputCaptcha = document.getElementById("captchaInput");
-    inputCaptcha.addEventListener("change", checkCaptcha);
+function captcha(){
+    let numerorandom = Math.floor((Math.random()*10000)+1);
+    let valorcaptcha = document.getElementById("captcha");
+    valorcaptcha.value = numerorandom;
 }
+
+captcha();
+
+function validar(){
+    event.preventDefault();
+    let captchausuario = document.getElementById("captcha-usuario");
+    let inputusuario = captchausuario.value;
+    let valorrandom = document.getElementById("captcha").value;
+
+    if (inputusuario == valorrandom){
+        alert("Captcha correcto");
+    }else{
+        alert("Captcha incorrecto");
+    }
+    captcha();
+}
+
+let btn = document.getElementById("enviar");
+btn.addEventListener("click", validar);
